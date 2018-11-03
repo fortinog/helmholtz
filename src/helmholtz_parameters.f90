bb7b2e57aae83e65db6009eccd0aaddbdc5d05eb
Fortino Garcia <fortino.garcia@colorado.edu>
2018-11-02 23:45:49 -0600

    Wave Eq Solver for 1D Dirichlet/Neumann BCs

--- src/helmholtz_parameters.f90
+++ src/helmholtz_parameters.f90
@@ -3,11 +3,50 @@ module helmholtz_parameters
  implicit none
  
  INTEGER,        PARAMETER :: dpp   = KIND(1.d0)
- REAL(KIND=dpp), PARAMETER :: omega = 10.d0  !wavenumber
+ REAL(KIND=dpp), PARAMETER :: OMEGA = 10.d0  !wavenumber
  REAL(KIND=dpp), PARAMETER :: PI    = ACOS(-1.d0)
-
+ REAL(KIND=dpp), PARAMETER :: SIGMA = 37.d0
  contains
 
+ !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ !! This subroutine takes in a boundary node and specifies
+ !! the Dirichlet boundary condition at time t. 
+ !! Inputs:
+ !! 	- x       : boundary node
+ !!     - t       : current time
+ !! Outputs:
+ !!		- u       : boundary value
+ !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ subroutine dirichlet_bc(x,t,u)
+
+  implicit none 
+
+  REAL(KIND=dpp), INTENT(IN)    :: x
+  REAL(KIND=dpp), INTENT(IN)    :: t
+  REAL(KIND=dpp), INTENT(INOUT) :: u
+
+  u = 0.d0
+ end subroutine dirichlet_bc
+
+ !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ !! This subroutine takes in a boundary node and specifies
+ !! the Neumann boundary condition at time t. 
+ !! Inputs:
+ !! 	- x       : boundary node
+ !!     - t       : current time
+ !! Outputs:
+ !!		- u       : boundary value
+ !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ subroutine neumann_bc(x,t,u)
+
+  implicit none 
+
+  REAL(KIND=dpp), INTENT(IN)    :: x
+  REAL(KIND=dpp), INTENT(IN)    :: t
+  REAL(KIND=dpp), INTENT(INOUT) :: u
+
+  u = 0.d0
+ end subroutine neumann_bc
 
  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
  !! This subroutine takes in a vector of spatial values and 
@@ -19,20 +58,26 @@ module helmholtz_parameters
  !!     - N       : number of grid points
  !! Outputs:
  !!		- u       : initial condition to a wave equation
+ !!     - ut      : initial velocity to a wave equation
+ !!				    FMG remove this for helmholtz solve?
  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
- subroutine initial_condition(x,N,u)
+ subroutine initial_condition(x,N,u,ut)
   
   implicit none 
   
-  REAL(KIND=dpp), intent(in)    :: x(:)
-  INTEGER,        intent(in)    :: N
-  REAL(KIND=dpp), intent(inout) :: u(:)
+  REAL(KIND=dpp), INTENT(IN)    :: x(:)
+  INTEGER,        INTENT(IN)    :: N
+  REAL(KIND=dpp), INTENT(INOUT) :: u(:)
+  REAL(KIND=dpp), INTENT(INOUT) :: ut(:)
 
   INTEGER :: i
   !! here we choose a constant
   ! u = 0.d0
   do i =1,N
-   u(i) = COS(0.5d0*x(i)*PI)
+   ! u(i) = COS(0.5d0*x(i)*PI)
+   ! u(i) = EXP(-SIGMA*x(i)**2.d0)
+   u(i) = COS(0.25d0*x(i)*PI) - SIN(0.25d0*x(i)*PI)
+   ut(i) = -1.d0*u(i)*PI*0.25d0
   end do 
 
  end subroutine initial_condition
@@ -50,9 +95,9 @@ module helmholtz_parameters
   
   implicit none 
   
-  REAL(KIND=dpp), intent(in)    :: x(:)
-  INTEGER,        intent(in)    :: N
-  REAL(KIND=dpp), intent(inout) :: c(:)
+  REAL(KIND=dpp), INTENT(IN)    :: x(:)
+  INTEGER,        INTENT(IN)    :: N
+  REAL(KIND=dpp), INTENT(INOUT) :: c(:)
 
   !! here we choose a constant
   c = 1.d0
@@ -71,9 +116,9 @@ module helmholtz_parameters
   
   implicit none 
   
-  REAL(KIND=dpp), intent(in)    :: x(:)
-  INTEGER,        intent(in)    :: N
-  REAL(KIND=dpp), intent(inout) :: rho(:)
+  REAL(KIND=dpp), INTENT(IN)    :: x(:)
+  INTEGER,        INTENT(IN)    :: N
+  REAL(KIND=dpp), INTENT(INOUT) :: rho(:)
 
   !! here we choose a constant
   rho = 1.d0
@@ -92,12 +137,21 @@ module helmholtz_parameters
   
   implicit none 
   
-  REAL(KIND=dpp), intent(in)    :: x(:)
-  INTEGER,        intent(in)    :: N
-  REAL(KIND=dpp), intent(inout) :: f(:)
+  REAL(KIND=dpp), INTENT(IN)    :: x(:)
+  INTEGER,        INTENT(IN)    :: N
+  REAL(KIND=dpp), INTENT(INOUT) :: f(:)
+
+  INTEGER        :: i
+  REAL(KIND=dpp) :: coeff
 
   !! here we choose a constant
   f = 0.d0
+
+  ! do i = 1,N
+  !  coeff = 4.d0*(SIGMA*x(i))**2.d0 + OMEGA**2.d0 - 2.d0*SIGMA
+  !  f(i) = coeff*EXP(-SIGMA*x(i)**2.d0)
+  ! end do
+
  end subroutine spatial_forcing
 
  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -113,14 +167,14 @@ module helmholtz_parameters
   
   implicit none 
   
-  REAL(KIND=dpp), intent(in)    :: x(:)
-  REAL(KIND=dpp), intent(in)    :: t
-  INTEGER,        intent(in)    :: N
-  REAL(KIND=dpp), intent(inout) :: g
+  REAL(KIND=dpp), INTENT(IN)    :: x(:)
+  REAL(KIND=dpp), INTENT(IN)    :: t
+  INTEGER,        INTENT(IN)    :: N
+  REAL(KIND=dpp), INTENT(INOUT) :: g
 
   !! here we choose a constant
   ! g = COS(omega*t)
-  g = 0.d0
+  g = 1.d0
  end subroutine temporal_forcing
 
  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -131,71 +185,80 @@ module helmholtz_parameters
  !! 	- c       : speed of sound in medium
  !!     - dx      : spatial step size
  !!     - N       : number of grid points
- !!     - BC      : 'D' for Dirichlet, 'N' for Neumann BCs
  !! Outputs:
  !!		- lap     : the laplacian 
  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
- subroutine compute_laplacian(u,c,dx,N,BC,lap)
+ subroutine compute_laplacian(u,c,dx,N,lap)
 
   implicit none
   
-  REAL(KIND=dpp),   intent(in)    :: u(:)
-  REAL(KIND=dpp),   intent(in)    :: c(:)
-  REAL(KIND=dpp),   intent(in)    :: dx
-  INTEGER,          intent(in)    :: N
-  CHARACTER(len=*), intent(in)    :: BC
-  REAL(KIND=dpp),   intent(inout) :: lap(:)
+  REAL(KIND=dpp),   INTENT(IN)    :: u(:)
+  REAL(KIND=dpp),   INTENT(IN)    :: c(:)
+  REAL(KIND=dpp),   INTENT(IN)    :: dx
+  INTEGER,          INTENT(IN)    :: N
+  REAL(KIND=dpp),   INTENT(INOUT) :: lap(:)
 
   INTEGER        :: i
   REAL(KIND=dpp) :: denom
 
   denom = 0.5d0/(dx**2.0d0)
 
-  if(BC .eq. 'D') then 
-
-   !! compute laplacian at left boundary
-   i = 2
-   lap(1) = SUM(c(i:i+1))*u(i+1) - SUM(c(i-1:i+1))*u(i)
-   lap(1) = lap(1) - c(i)*u(i)
-   lap(1) = denom*lap(1)
-
-   !! Account for Dirichlet boundary conditions
+  lap(1) = 0.d0
+  do i=2,N+1
    lap(i) = SUM(c(i:i+1))*u(i+1) - SUM(c(i-1:i+1))*u(i)
-   lap(i) = lap(i) - c(i)*u(i)
-   lap(i) = denom*lap(i)
-
-   !! compute laplacian on nodes with support 
-   !! contained in the interior of the domain
-   do i=3,N-2
-    lap(i) = SUM(c(i:i+1))*u(i+1) - SUM(c(i-1:i+1))*u(i)
-    lap(i) = lap(i) - c(i)*u(i) + SUM(c(i-1:i))*u(i-1)
-    lap(i) = denom*lap(i)
-   end do 
-
-   i = N-1
-   !! Account for Dirichlet boundary conditions
-   lap(i) = -1.d0*SUM(c(i-1:i+1))*u(i)
    lap(i) = lap(i) - c(i)*u(i) + SUM(c(i-1:i))*u(i-1)
    lap(i) = denom*lap(i)
-
-   !! compute laplacian at right boundary
-   lap(N) = -1.d0*SUM(c(i-1:i+1))*u(i)
-   lap(N) = lap(N) - c(i)*u(i) + SUM(c(i-1:i))*u(i-1)
-   lap(N) = denom*lap(N)
-
-  elseif(BC .eq. 'N') then 
-   
-   write(*,*) "This boundary condition is not supported."
-   stop 234
-  
-  else 
-   
-   write(*,*) "This boundary condition is not supported."
-   stop 123
-  
-  end if
+  end do
+  lap(N+2) = 0.d0 
 
  end subroutine compute_laplacian
 
+ !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ !! This subroutine takes in the current solution vector u
+ !! and enforces the desired boundary conditions (this
+ !! is to be modified if different conditions are desired).
+ !! Inputs:
+ !!     - dx      : spatial step size
+ !!     - N       : number of interior grid points
+ !!     - BC      : 'D' for Dirichlet, 'N' for Neumann BCs
+ !!     - t       : currrent time
+ !! Outputs:
+ !! 	- u       : current solution
+ !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ subroutine enforce_bcs(x,dx,N,BC,t,u)
+  
+  implicit none 
+  
+  REAL(KIND=dpp),   INTENT(IN)    :: x(:)
+  REAL(KIND=dpp),   INTENT(IN)    :: dx
+  INTEGER,          INTENT(IN)    :: N
+  CHARACTER(len=*), INTENT(IN)    :: BC
+  REAL(KIND=dpp),   INTENT(IN)    :: t
+  REAL(KIND=dpp),   INTENT(INOUT) :: u(:)
+
+  INTEGER        :: i
+  REAL(KIND=dpp) :: val
+
+  SELECT CASE (BC)
+  	CASE('DD')
+  		!! Purely Dirichlet data
+  		CALL dirichlet_bc(x(2),t,val)
+  		u(2)   = val
+  		CALL dirichlet_bc(x(N+1),t,val)
+  		u(N+1) = val
+  	CASE('NL')
+  		!! Neumann condition on left
+  		CALL neumann_bc(x(2),t,val)
+  		u(1)   = u(3) - 2.d0*dx*val
+   		CALL dirichlet_bc(x(N+1),t,val)
+  		u(N+1) = val
+  	CASE('NR')
+  		!! Neumann condition on right
+   		CALL dirichlet_bc(x(2),t,val)
+  		u(2)   = val
+  		CALL neumann_bc(x(N+1),t,val)
+  		u(N+2) = u(N) + 2.d0*dx*val
+  	END SELECT
+ end subroutine enforce_bcs
 
 end module helmholtz_parameters
--- src/helmholtz_wave_fd.f90
+++ src/helmholtz_wave_fd.f90
@@ -2,15 +2,17 @@ program helmholtz_wave_fd
  use helmholtz_parameters
  implicit none
 
- INTEGER,        PARAMETER :: N = 100
- REAL(KIND=dpp), PARAMETER :: Tend = 2.0d0/omega
+ INTEGER,        PARAMETER      :: N = 100
+ REAL(KIND=dpp), PARAMETER      :: Tend = 2.0d0/omega
  
- REAL(KIND=dpp)     :: x(N), u(N), rho(N), ut(N), utt(N)
- REAL(KIND=dpp)     :: v(N), c(N), f(N), up(N), un(N)
- REAL(KIND=dpp)     :: dx, dt, dt2, t
- REAL(KIND=dpp)     :: a, b, CFL, g
- INTEGER            :: i, j, nsteps
- CHARACTER(len=1)   :: BC
+ REAL(KIND=dpp), ALLOCATABLE :: x(:),u(:),rho(:),ut(:)
+ REAL(KIND=dpp), ALLOCATABLE :: utt(:),v(:),c(:),f(:),up(:)
+ REAL(KIND=dpp), ALLOCATABLE :: un(:), sol(:)
+ 
+ REAL(KIND=dpp)     			:: dx, dt, dt2, t
+ REAL(KIND=dpp)     			:: a, b, CFL, g
+ INTEGER                        :: i, j, nsteps
+ CHARACTER(len=2)               :: BC
 
  !! Set up domain
  a      = -1.0d0 !left endpt
@@ -21,22 +23,26 @@ program helmholtz_wave_fd
  dt2    = dt**2.d0
  nsteps = INT(Tend/dt)
  t      = 0.d0
- BC     = 'D'    !Dirichlet boundary condition
+ BC     = 'NL'    ! Boundary Condition
 
- !! spatial grid
- do i = 0,N-1
-  x(i+1) = a + REAL(i,dpp)*dx
+ !! Allocate working arrays
+ ALLOCATE(x(N+2), u(N+2), rho(N+2), ut(N+2), utt(N+2))
+ ALLOCATE(v(N+2), c(N+2), f(N), up(N+2), un(N+2), sol(N+2))
+
+ !! spatial grid (include ghost points)
+ do i = -1,N
+  x(i+2) = a + REAL(i,dpp)*dx
  end do
 
  !! get initial conditions and physical constants
- CALL initial_condition(x,N,v)
- CALL medium_speed(x,N,c)
- CALL density(x,N,rho)
- CALL spatial_forcing(x,N,f)
- CALL temporal_forcing(x,t,N,g)
+ CALL initial_condition(x,N+2,v,ut)
+ CALL medium_speed(x,N+2,c)
+ CALL density(x,N+2,rho)
+ CALL spatial_forcing(x,N+2,f)
+ CALL temporal_forcing(x,t,N+2,g)
 
  !! Calculate RHS of equation at t=0
- CALL compute_laplacian(v,c,dx,N,BC,utt)
+ CALL compute_laplacian(v,c,dx,N,utt)
 
  !! Calculate u_{tt} at t=0 
  do i = 1,N
@@ -44,8 +50,10 @@ program helmholtz_wave_fd
  end do
 
  !! Via a Taylor expansion, we note that 
- !! u(-dt) = v + 0.5*dt^2*u_{tt}
- up = v + 0.5d0*utt*dt2
+ !! u(-dt) = v - dt*ut 0.5*dt^2*u_{tt}
+ !! FMG: We may remove the dt*ut term 
+ !! for Helmholtz solve
+ up = v + 0.5d0*utt*dt2 - dt*ut
  u = v
 
  !! March forward in time. Here we use 
@@ -58,7 +66,8 @@ program helmholtz_wave_fd
   
   !! compute quantities at next time level
   CALL temporal_forcing(x,t,N,g)
-  CALL compute_laplacian(un,c,dx,N,BC,utt)
+  CALL enforce_bcs(x,dx,N,BC,t,un)
+  CALL compute_laplacian(un,c,dx,N,utt)
   
   !! calculate u_{tt} at current time
   do j = 1,N
@@ -68,20 +77,37 @@ program helmholtz_wave_fd
   !! overwrite solutions
   up = u
   u = un
+
+  do j = 2,N+1 
+  	sol(j) = v(j)*(COS(0.25*PI*t) - SIN(0.25*PI*t))
+  end do 
+  !! Output error at each time step
+  write(*,'((E24.16))') MAXVAL(ABS(u(2:N+1) - sol(2:N+1)))
  end do 
 
  !! write data to file
  open(2,file='x.txt',status='unknown')
- do i=1,N
+ do i=2,N+1
   write(2,fmt='((E24.16))') x(i)
  end do
  close(2)
 
  open(3,file='u.txt',status='unknown')
- do i=1,N
+ do i=2,N+1
   write(3,fmt='((E24.16))') u(i)
  end do
  close(3)
 
+ !! Deallocate all working arrays
+ DEALLOCATE(un)
+ DEALLOCATE(up)
+ DEALLOCATE(f)
+ DEALLOCATE(c)
+ DEALLOCATE(v)
+ DEALLOCATE(utt)
+ DEALLOCATE(ut)
+ DEALLOCATE(rho)
+ DEALLOCATE(u)
+ DEALLOCATE(x)
 
 end program helmholtz_wave_fd
